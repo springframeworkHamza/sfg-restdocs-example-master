c9b6958645ca0e4cb6fb34736b8dcb619edfcfe1
springframeworkHamza <kizotomandera@hotmail.com>
2021-05-26 16:56:15 +0200

    doc responses JSON

--- sfg-restdocs-example-master/src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ sfg-restdocs-example-master/src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -26,6 +26,8 @@ import static org.mockito.BDDMockito.given;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -58,6 +60,17 @@ class BeerControllerTest {
                         ),
                         requestParameters(
                                 parameterWithName("iscold").description("Is beer Cold Query param")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("Id of Beer"),
+                                fieldWithPath("version").description("Version number"),
+                                fieldWithPath("createdDate").description("Date Created"),
+                                fieldWithPath("lastModifiedDate").description("Date Updated"),
+                                fieldWithPath("beerName").description("Beer Name"),
+                                fieldWithPath("beerStyle").description("Beer Style"),
+                                fieldWithPath("upc").description("Price"),
+                                fieldWithPath("price").description("Quantity On hand"),
+                                fieldWithPath("quantityOnHand").description("Id of Beer")
                         )));
     }
 
